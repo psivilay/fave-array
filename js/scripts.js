3b5a73acac1fff32fe1247dddf8d908d51a8c3c9
Peter Sivilay <sivilaypeter@gmail.com>
2016-12-02 21:41:42 -0600

    Add loop to put array into list

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,43 @@ $(document).ready(function() {
   $("form#fave-things").submit(function(event){
     event.preventDefault();
 
-    var faveHero = $("input:radio[name=hero]:checked").val();
-    var faveColor = $("#color").val();
-    var faveSeason = $("#season").val();
-    var faveFood = $("#food").val();
 
-    var userArray = [faveHero, faveColor, faveSeason, faveFood]
+//    var faveHero = $("#hero").val();
+//    var faveColor = $("#color").val();
+//    var faveSeason = $("#season").val();
+//    var faveFood = $("#food").val();
 
+    var userFaves = ["hero", "color", "season", "food"];
     var newArray = [];
 
-    newArray.push(userArray[1]);
-    newArray.push(userArray[0]);
-    newArray.push(userArray[2]);
-    newArray.push(userArray[3]);
+    newArray.push(userFaves[1]);
+    newArray.push(userFaves[0]);
+    newArray.push(userFaves[2]);
+    newArray.push(userFaves[3]);
+
+    newArray.forEach(function(userFave) {
+      var userInput = $("#" + userFave).val();
+//Everything works up until below:
+      $("." + userFave).text(userInput);
+
+    });
+
+    $("ul").show();
+
+//    var userArray = [faveHero, faveColor, faveSeason, faveFood]
+
+//    var newArray = [];
+
+//    newArray.push(userArray[1]);
+//    newArray.push(userArray[0]);
+//    newArray.push(userArray[2]);
+//    newArray.push(userArray[3]);
+
+// var newArray should now be [faveColor, faveHero, faveSeason, faveFood]
+
+//    $("ul#output").append(newArray);
 
 
-
-    $("#output").text(newArray);
 
   });
 });
